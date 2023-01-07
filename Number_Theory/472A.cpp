cb9268e7538fe56532c4c1f27280c384a49aa1b0
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-01-07 13:13:09 +0600

    472A. Design Tutorial: Learn from Math

---
+++ Number_Theory/472A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+#define ll long long
+using namespace std;
+
+int main() 
+{
+    ll n;
+    cin>>n;
+    if(n%2){
+      cout<<9<<" "<<n-9<<endl
+    }
+    else{
+      cout<<4<<" "<<n-4<<endl
+    }
+    return 0;
+}
