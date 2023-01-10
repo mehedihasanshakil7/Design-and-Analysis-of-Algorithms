6034a2348869aceb627e3a8bb3b4ce30d1641736
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-01-10 23:20:28 +0600

    1370A. Maximum GCD

---
+++ Number_Theory/1370A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+#define ll long long
+using namespace std;
+
+int main() 
+{
+    ll t;
+    cin>>t;
+    while(t--) {
+      ll n;
+      cin>>n;
+      cout<<n/2<<endl
+    }
+    return 0;
+}
