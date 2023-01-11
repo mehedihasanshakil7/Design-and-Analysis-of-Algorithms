f3fb6ffb82402b693485539f9eb8fa22fe8623e3
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-01-11 10:40:26 +0600

    1455A. Strange Functions

---
+++ Number_Theory/1455A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+#define ll long long
+using namespace std;
+int main() 
+{
+    ll t;
+    cin>>t;
+    while(t--) {
+      string s;
+      cin>>s;
+      cout<<s.length()<<endl
+    }
+    return 0;
+}
