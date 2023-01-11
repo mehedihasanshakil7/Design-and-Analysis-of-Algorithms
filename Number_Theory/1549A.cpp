2636cbd95ecc495d37a8e8ca9be49cf28856a35e
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-01-11 10:20:32 +0600

    1549A. Gregor and Cryptography

---
+++ Number_Theory/1549A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+#define ll long long
+using namespace std;
+int main() 
+{
+    ll t;
+    cin>>t;
+    while(t--) {
+      ll n;
+      cin>>n;
+      cout<<2<<" "<<n-1<<endl
+    }
+    return 0;
+}
