ced5e5f700d9731d0a74b3b7c08d9f5f15633ecc
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-01-11 10:29:26 +0600

    1553A. Digits Sum

---
+++ Number_Theory/1553A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+#define ll long long
+using namespace std;
+int main() 
+{
+    ll t;
+    cin>>t;
+    while(t--) {
+      ll n;
+      cin>>n;
+      cout<<(n+1)/10<<endl
+    }
+    return 0;
+}
