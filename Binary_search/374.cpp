4b3a699f24a921b8df3c95b42e24166840471970
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-01-18 21:02:22 +0600

    374. Guess Number Higher or Lower (leetcode)

---
+++ Binary_search/374.cpp
@@ -0,0 +1,27 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long long l = 1, r = n, mid;
+        while(1) {
+            mid = (l + r) / 2;
+            if(guess(mid) == -1) {
+                r = mid - 1;
+            }
+            else if(guess(mid) == 1) {
+                l = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+    }
+};
