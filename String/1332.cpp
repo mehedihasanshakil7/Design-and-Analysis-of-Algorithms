85b2f99165cfce1f4bd626abb61a8ec8b19ca5c5
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-02-07 23:53:55 +0600

    1332. Remove Palindromic Subsequences

---
+++ String/1332.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int n = s.size();
+        if(n == 0) return 0;
+        int i = 0, j = n - 1;
+        while(i < j) {
+            if(s[i] != s[j]) return 2;
+            i++; j--;
+        }
+        return 1;
+    }
+};
