241116b0fadf14ea8cb5759bb1f4e35d9f4ebd84
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-02-13 00:57:12 +0600

    1791D. Distinct Split

--- String/1791D.cpp
+++ String/1791D.cpp
@@ -1,52 +1,32 @@
 #include <bits/stdc++.h>
 #define endl "\n";
-#define ll long long
 #define pb push_back
 using namespace std;
 
-void fastIO() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-}
-
-void solve() {
-  
-  int t;
-  cin>>t;
-  
-  while(t--) {
-    
-    int n;
-    cin>>n;
-    
-    string s;
-    cin>>s;
-    
-    set<char> s1, s2;
-    
-    vector<int> v1(n+1), v2(n+1);
-    
-    for(int i = 0; i < n; i++) {
-      s1.insert(s[i]);
-      v1[i+1] = s1.size();
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--) {
+      int n;
+      cin>>n;
+      string s;
+      cin>>s;
+      set<char> s1, s2;
+      vector<int> v1, v2;
+      for(int i = 0; i < n-1; i++) {
+        s1.insert(s[i]);
+        s2.insert(s[n-i-1]);
+        v1.pb(s1.size());
+        v2.pb(s2.size());
+      }
+      int max = 0, ans = 0, m = v1.size();
+      for(int i = 0; i < m; i++) {
+        if(v1[i] + v2[m-i-1] > ans) {
+          ans = v1[i] + v2[m-i-1];
+        }
+      }
+      cout<<ans<<endl
     }
-    for(int i = n-1; i >= 0; i--) {
-      s2.insert(s[i]);
-      v2[n-i] = s2.size();
-    }
-    int ans = 0;
-    for(int i = 0; i <= n; i++) {
-      ans = max(v1[i]+v2[n-i], ans);
-    }
-    cout<<ans<<endl
-  }
-}
-
-
-int main() {
-  fastIO();
-  
-  solve();
-  return 0;
+    return 0;
 }
