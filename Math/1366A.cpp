c1ab1028efa952ad738ebdae437c323d8da8edaa
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2023-02-17 13:11:45 +0600

    1366A. Shovels and Swords

---
+++ Math/1366A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--) {
+      int a, b;
+      cin>>a>>b;
+      cout<<min({a, b, (a+b)/3})<<endl
+    }
+    return 0;
+}
